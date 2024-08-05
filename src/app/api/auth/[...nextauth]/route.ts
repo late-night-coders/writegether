15dc3269cf85e249ac1025032560f1e199fbb723
KatelynKim <wlsdud194@hotmail.com>
2024-08-04 17:48:40 -0700

    NextAuth Google Provider setup

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth"
+import NextAuth from "next-auth/next"
+import GoogleProvider from 'next-auth/providers/google'
+
+const authConfig: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ]
+}
+
+const handler = NextAuth(authConfig)
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client"
+import { signIn } from "next-auth/react"
+
+function GoogleSignInButton () {
+  const handleClick = () => {
+    signIn("google")
+  }
+  return (
+    <button
+      onClick={handleClick}
+    > Google Login</button>
+  )
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <GoogleSignInButton />
     </main>
-  );
+  )
 }
