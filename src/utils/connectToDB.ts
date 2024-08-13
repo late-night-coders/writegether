0c8997094f77f7835d478570819cc8f462777f0f
KatelynKim <wlsdud194@hotmail.com>
2024-08-13 01:17:51 -0700

    Set up DB connection

---
+++ src/utils/connectToDB.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+export async function connectToDB() {
+  if (mongoose.connection.readyState !== 1) {
+    try {
+      await mongoose.connect(process.env.MONGO_DB_URL!)
+      console.log('db is connected')
+    } catch (error) {
+      console.error('error ocnnecting to db', error)
+    }
+  } else {
+    console.log('db is already connected')
+  }
+}
+
+export default connectToDB()
