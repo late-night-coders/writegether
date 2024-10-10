1afaa5f2353186e06ed855717a29a5397c1fd9fd
MikeyOnyedika <michaelvik520@gmail.com>
2024-10-10 18:17:30 +0100

    make signup page responsive on desktop view

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,9 +3,9 @@ import Header from "@/app/(auth)/components/Header"
 
 export default function AuthPageLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="w-full h-full flex flex-col gap-8">
+    <main className="w-full h-full flex flex-col gap-8 items-center">
       <Header />
-      <section className="w-full h-full flex p-4">
+      <section className="w-full h-full flex p-4 sm:max-w-[23.5rem]">
         {children}
       </section>
     </main>
