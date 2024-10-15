a647302cdd0011100b9d66e76d56e308403d0ed0
MikeyOnyedika <michaelvik520@gmail.com>
2024-10-15 14:15:50 +0100

    add test to make sure next-auth 'signIn' function triggers on google signin button click

--- __tests__/components/NavigationBar/SignInButton.tsx
+++ __tests__/components/NavigationBar/SignInButton.tsx
@@ -1,5 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import GoogleSignInButton from '@/components/NavigationBar/SignInButton'
+import { signIn } from "next-auth/react"
+
+jest.mock("next-auth/react") // mock the next-auth module
 
 describe('SigInButton', () => {
     it("renders a button", () => {
@@ -8,5 +11,16 @@ describe('SigInButton', () => {
         const button = screen.getByRole("button", { name: "LOGIN" })
 
         expect(button).toBeInTheDocument()
+    })
+
+    it("triggers google signin when clicked", () => {
+        // render the component
+        const { getByText } = render(<GoogleSignInButton />)
+        // get a reference to the component
+        const button = getByText("LOGIN")
+        // simulate a click on the button
+        fireEvent.click(button)
+        // assert that the (mocked) signIn function was called with the string argument "google"
+        expect(signIn).toHaveBeenCalledWith("google")
     })
 })
