a28f855594472b689794dffbd3ace8d1261535b6
MikeyOnyedika <michaelvik520@gmail.com>
2024-10-24 19:42:54 +0100

    remove unncessary code comments

--- __tests__/components/NavigationBar/SignInButton.tsx
+++ __tests__/components/NavigationBar/SignInButton.tsx
@@ -2,25 +2,19 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import GoogleSignInButton from '@/components/NavigationBar/SignInButton'
 import { signIn } from "next-auth/react"
 
-jest.mock("next-auth/react") // mock the next-auth module
+jest.mock("next-auth/react")
 
 describe('SigInButton', () => {
     it("renders a button", () => {
         render(<GoogleSignInButton />)
-
         const button = screen.getByRole("button", { name: "LOGIN" })
-
         expect(button).toBeInTheDocument()
     })
 
     it("triggers google signin when clicked", () => {
-        // render the component
         const { getByText } = render(<GoogleSignInButton />)
-        // get a reference to the component
         const button = getByText("LOGIN")
-        // simulate a click on the button
         fireEvent.click(button)
-        // assert that the (mocked) signIn function was called with the string argument "google"
         expect(signIn).toHaveBeenCalledWith("google")
     })
 })
--- jest.config.ts
+++ jest.config.ts
@@ -2,36 +2,19 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // path to nextjs app to load next.config.js and .env from
   dir: "./",
 })
 
 const config: Config = {
-
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
-
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
   },
-
-  // A preset that is used as a base for Jest's configuration
   preset: "ts-jest",
-
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-
-  // The test environment that will be used for testing
   testEnvironment: "jsdom",
 }
 
